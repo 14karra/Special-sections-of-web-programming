3ca96ada3d344a7f285e4a7b935d5fc2d7a05235
Ханк <hankdjambong@gmail.com>
2020-10-04 18:53:19 +0300

    Lab3 - Пятняшки

--- js/fifteenModel.js
+++ js/fifteenModel.js
@@ -1,13 +1,14 @@
 let Model = function () {
     this.movements = {
-        39: 'left',
-        37: 'right',
-        40: 'up',
-        38: 'down'
+        39: {'name': 'left', 'move': (sound) => { this.go(this.Move.left, sound); } },
+        37: {'name': 'right', 'move': (sound) => { this.go(this.Move.right, sound); } },
+        40: {'name': 'up', 'move': (sound) => { this.go(this.Move.up, sound); } },
+        38: {'name': 'down', 'move': (sound) => { this.go(this.Move.down, sound); } }
     };
+    this.squareSize = 4;
 };
 Model.prototype.init = function (needRendering, gameWon) {
-    this.Move = {up: -4, left: -1, down: 4, right: 1};
+    this.Move = {up: -1 * this.squareSize, left: -1, down: this.squareSize, right: 1};
     this.order = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].sort(function () {
         return Math.random() - .5;
     }).concat(0);
@@ -23,8 +24,7 @@ Model.prototype.checkIsCompleted = function () {
 Model.prototype.go = function (move, sound) {
     var index = this.hole + move;
     if (!this.order[index]) return false;
-    if (move === this.Move.left || move === this.Move.right)
-        if (Math.floor(this.hole / 4) !== Math.floor(index / 4)) return false;
+    if (this.movementImpossible(move, index)) return false;
     this.swap(index, this.hole);
     this.hole = index;
     sound.play();
@@ -35,6 +35,10 @@ Model.prototype.go = function (move, sound) {
     }
     return true;
 };
+Model.prototype.movementImpossible = function (move, index) {
+    return (move === this.Move.left || move === this.Move.right) &&
+        (Math.floor(this.hole / this.squareSize) !== Math.floor(index / this.squareSize));
+};
 Model.prototype.swap = function (i1, i2) {
     var t = this.order[i1];
     this.order[i1] = this.order[i2];
@@ -42,20 +46,7 @@ Model.prototype.swap = function (i1, i2) {
 };
 Model.prototype.keyPress = function (e, sound) {
     if (e.keyCode in this.movements) {
-        switch (e.keyCode) {
-            case 39:
-                this.go(this.Move.left, sound);
-                break;
-            case 37:
-                this.go(this.Move.right, sound);
-                break;
-            case 40:
-                this.go(this.Move.up, sound);
-                break;
-            case 38:
-                this.go(this.Move.down, sound);
-                break;
-        }
+        this.movements[e.keyCode].move(sound);
     }
 };
 
