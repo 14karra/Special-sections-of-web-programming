9655851ec03e978e45ff69fc4a812b615c835ad4
Ханк <hankdjambong@gmail.com>
2020-12-07 10:59:06 +0300

    Lab8 - SPA with SVG - fixed.

--- js/constants.js
+++ js/constants.js
@@ -1,2 +1,4 @@
 const SQUARE_SIZE = 4;
 const PIECE_SIDE = 100;
+const MIN_COLOR_BASE = 16;
+const XML_NS = "http://www.w3.org/2000/svg";
--- js/fifteenView.js
+++ js/fifteenView.js
@@ -19,38 +19,44 @@ View.prototype.render = function (objects) {
         this.mainScene.style.backgroundColor = 'cornsilk';
     }
 
-    let xmlns = "http://www.w3.org/2000/svg";
-
     for (let columnIndex = 0; columnIndex < SQUARE_SIZE; columnIndex++) {
         for (let lineIndex = 0; lineIndex < SQUARE_SIZE; lineIndex++) {
             let orderIndex = lineIndex * SQUARE_SIZE + columnIndex;
 
             if (orderIndex === objects.hole) continue;
 
-            let r = 16 * objects.order[orderIndex];
+            let r = MIN_COLOR_BASE * objects.order[orderIndex];
             let piece_X = columnIndex * PIECE_SIDE + 20;
             let piece_y = lineIndex * PIECE_SIDE + 20;
 
-            let someRect = document.createElementNS(xmlns, 'rect');
-            someRect.setAttribute('x', String(piece_X));
-            someRect.setAttribute('y', String(piece_y));
-            someRect.setAttribute('rx', '10');
-            someRect.setAttribute('ry', '10');
-            someRect.setAttribute('width', String(PIECE_SIDE));
-            someRect.setAttribute('height', String(PIECE_SIDE));
-            someRect.setAttribute('style', 'fill: rgb(' + r + ',' + r + ',' + r + ');' +
-                'stroke: black;stroke-width: 5;opacity: 0.5;');
-            this.mainScene.appendChild(someRect);
+            this.mainScene.appendChild(createRectPiece(piece_X, piece_y, r));
 
-            let squareNumber = document.createElementNS(xmlns, 'text');
-            squareNumber.setAttribute('x', String(piece_X + (PIECE_SIDE / 2) - 10));
-            squareNumber.setAttribute('y', String(piece_y + (PIECE_SIDE / 2) + 10));
-            squareNumber.setAttribute('fill', 'rgb(16,19,27)');
-            squareNumber.style.fontSize = "30px";
-            squareNumber.innerHTML = objects.order[orderIndex];
-            this.mainScene.appendChild(squareNumber);
+            this.mainScene.appendChild(createNbrText(piece_X, piece_y, objects.order[orderIndex]));
         }
     }
 };
+
+function createRectPiece(piece_x, piece_y, r) {
+    let someRect = document.createElementNS(XML_NS, 'rect');
+    someRect.setAttribute('x', String(piece_x));
+    someRect.setAttribute('y', String(piece_y));
+    someRect.setAttribute('rx', '10');
+    someRect.setAttribute('ry', '10');
+    someRect.setAttribute('width', String(PIECE_SIDE));
+    someRect.setAttribute('height', String(PIECE_SIDE));
+    someRect.setAttribute('style', 'fill: rgb(' + r + ',' + r + ',' + r + ');' +
+        'stroke: black;stroke-width: 5;opacity: 0.5;');
+    return someRect;
+}
+
+function createNbrText(piece_x, piece_y, nbr) {
+    let squareNumber = document.createElementNS(XML_NS, 'text');
+    squareNumber.setAttribute('x', String(piece_x + (PIECE_SIDE / 2) - 10));
+    squareNumber.setAttribute('y', String(piece_y + (PIECE_SIDE / 2) + 10));
+    squareNumber.setAttribute('fill', 'rgb(16,19,27)');
+    squareNumber.style.fontSize = "30px";
+    squareNumber.innerHTML = nbr;
+    return squareNumber;
+}
 
 let fifteenView = new View();
